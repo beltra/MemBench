84ac13849077ffc6363fa67f657e1d1af1e0097a
beltra <gbeve2002@gmail.com>
2023-01-11 23:43:28 +0100

    Reorganized folders

--- ddrbench_sol/script.tcl
+++ ddrbench_sol/script.tcl
@@ -3,16 +3,16 @@
 ## Please DO NOT edit it.
 ## Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ############################################################
-open_project ddrBench
+open_project MemBench
 set_top ddrBenchmark
 add_files ddrBench/src/ddrbenchmark.hpp
 add_files ddrBench/src/ddrbenchmark.cpp
-add_files -tb ddrBench/src/testbench/ddrbenchmark_testbench.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
+add_files -tb MemBench/src/testbench/ddrbenchmark_testbench.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "ddrbench_sol" -flow_target vivado
 set_part {xc7z020-clg400-1}
 create_clock -period 10 -name default
 config_export -format ip_catalog -rtl verilog
-source "./ddrBench/ddrbench_sol/directives.tcl"
+source "./MemBench/ddrbench_sol/directives.tcl"
 csim_design -clean
 csynth_design
 cosim_design
